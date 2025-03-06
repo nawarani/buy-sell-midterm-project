4ce221a47c1e6c8cbaf7dd92d47afde7adff7d79
Anika Nawar <nawarani47@gmail.com>
2025-03-05 22:35:42 -0500

    all 5 tables created, schema done

--- db/schema/06_messages.sql
+++
@@ -1,7 +0,0 @@
--- Drop and recreate Widgets table (Example)
-
-DROP TABLE IF EXISTS messages CASCADE;
-CREATE TABLE messages (
-  id SERIAL PRIMARY KEY NOT NULL,
-  room_id INTEGER REFERENCES message_room(id) ON DELETE CASCADE
-);

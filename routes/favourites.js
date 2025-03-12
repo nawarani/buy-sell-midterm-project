c731bed1c792569cfcd67245bdd7682b8f1c416c
Anika Nawar <nawarani47@gmail.com>
2025-03-12 02:02:27 -0400

    /favorites shows item and posts to the database, the db function not created yet

--- routes/favourites.js
+++ routes/favourites.js
@@ -2,16 +2,30 @@ const express = require('express');
 const router  = express.Router();
 const favouritesQueries = require('../db/queries/favourites');
 
-
 router.get('/', (req, res) => {
-  favouritesQueries.getFavouritesById(req.session.userId)
-  .then((data) => {
-    const templateVars = {
-      userId: req.session.userId,
-      favouriteItems: data
-    };
-    res.render('favourites', templateVars);
-  });
+  const userId = req.session.userId;
+  if (userId) {
+    favouritesQueries.getFavouritesById(req.session.userId)
+    .then((data) => {
+      const templateVars = {
+        userId: req.session.userId,
+        favouriteItems: data
+      };
+      res.render('favourites', templateVars);
+    });
+  } else {
+    res.redirect('/login');
+  }
+}); 
+
+router.post('/', (req, res) => {
+  const userId = req.session.userId;
+  if (userId) {
+    const game_id = 2; // for testing
+    favouritesQueries.markFavourite(userId, game_id);
+  } else {
+    res.redirect('/login');
+  }
 }); 
 
 module.exports = router;

a1999ec1322988e7c4a44a05e12ec67293fba0b4
CodeJuuun <Chomseng.a@gmail.com>
2025-03-13 01:29:00 -0400

    finally got a working filter function that connects to the DB and will filter out games based on the value

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,12 +1,26 @@
 // this makes the price slider active and can see the price change for the filter
 
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const priceSlider = document.getElementById("maxPrice");
   const priceValue = document.getElementById("priceValue");
+  const filterform = document.querySelector('form'); 
 
   priceValue.textContent = priceSlider.value;
 
   priceSlider.addEventListener("input", function() {
     priceValue.textContent = priceSlider.value;
+  })
+
+  filterform.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    // updates the query parameter via form action
+    const currentValue = priceSlider.value;
+    filterform.action = `/games?maxPrice=${currentValue}`;
+
+
+      filterform.submit();
   })
 });
--- routes/games.js
+++ routes/games.js
@@ -96,4 +96,26 @@ db.query(resetQuery)
       });
   });
 
+
+  // this will filter out games based on the price filtering feature
+gameRoutes.get("/", (req, res) => {
+  const maxPrice = req.query.maxPrice || 200; //default price if non is set
+
+  // querying games in combination with the price filter
+  const query = `
+  SELECT * FROM games
+  WHERE price_cents <= $1
+  ORDER BY is_sold
+  `;
+
+  db.query(query, [maxPrice * 100]) //converts prices
+    .then((result) => {
+      const templateVars = { games: result.rows};
+      res.render("index", templateVars)
+    })
+    .catch((err) => {
+      console.error("Error", err);
+      res.status(500).send("error loading games")
+    });
+})
 module.exports = gameRoutes

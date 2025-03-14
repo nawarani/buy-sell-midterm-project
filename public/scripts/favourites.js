df96183c23b232ab1162d354d158abd5be9f23b1
CodeJuuun <Chomseng.a@gmail.com>
2025-03-14 12:08:48 -0400

    all script files linted

--- public/scripts/favourites.js
+++ public/scripts/favourites.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
     const game_id = $(event.target).data().favourite;
     console.log('fav jquery:', game_id);
     $.post(`/favourites?game_id=${game_id}`)
-    .then((res) => {
-      // be more specific in targetting
-      $(event.target).find('.fa-heart').addClass('fa-heart-clicked');
-    });
+      .then((res) => {
+        // be more specific in targetting
+        $(event.target).find('.fa-heart').addClass('fa-heart-clicked');
+      });
   });
 });
--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
       },
       success: function(res) {
         $('.message-container').empty();
-        for(let message of res.messages) {
+        for (let message of res.messages) {
           $('.message-container').append(`<div class='message sender-${message.sender_id}'> ${message.text} </div>`);
         }
         $(`.sender-${res.userId}`).addClass('sender-messages');
@@ -35,7 +35,7 @@ $(document).ready(function() {
       error: function(xhr, status, error) {
         console.log('error inside load message ajax request:', error);
       }
-    })
+    });
   };
 
 

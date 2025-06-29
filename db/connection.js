eee30ff86e90ca657d3480229a0438a5062af07b
Anika Nawar <nawarani47@gmail.com>
2025-06-28 21:48:23 -0400

    changed db setup for render deploy

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,20 @@
 // PG database client/connection setup
 const { Pool } = require('pg');
 
-const dbParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-};
+// const dbParams = {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME
+// };
+// const db = new Pool(dbParams);
 
-const db = new Pool(dbParams);
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+});
+
 
 db.connect();
 

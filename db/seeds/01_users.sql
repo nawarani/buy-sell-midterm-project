8eb21b176d4199f3f4ac8c2ac6689dd5fd7efb09
Anika Nawar <nawarani47@gmail.com>
2025-06-28 22:38:06 -0400

    login workaround

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name, email, is_admin) VALUES ('Admin', 'admin@test.com', true);
 INSERT INTO users (name, email, is_admin) VALUES ('Buyer', 'buyer@test.com', false);
+INSERT INTO users (name, email, is_admin) VALUES ('Test', 'test@test.com', false);
